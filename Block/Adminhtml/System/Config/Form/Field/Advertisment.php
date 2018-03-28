c50de588136a12759d2e3d4fa895d0f582010338
SlavaYurthev <slavik-iii@ukr.net>
2018-03-28 22:17:16 +0300

    0.1.0

---
+++ Block/Adminhtml/System/Config/Form/Field/Advertisment.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Testimonials
+ * 
+ * @author Slava Yurthev
+ */
+namespace SY\Testimonials\Block\Adminhtml\System\Config\Form\Field;
+
+use \Magento\Framework\Data\Form\Element\AbstractElement;
+
+class Advertisment extends \Magento\Config\Block\System\Config\Form\Field {
+	protected function _getElementHtml(AbstractElement $element){
+		return '';
+	}
+}
